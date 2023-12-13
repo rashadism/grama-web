f94f7a29aad190335f15a92100987b5af3e98baa
Mohamed Rashad <rashad@wso2.com>
2023-12-13 16:22:31 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import { useViewContext } from "./contexts/ViewContext";
+import Police from "./components/Police";
+import Identity from "./components/Identity";
+import Address from "./components/Address";
+import NotFound from "./components/NotFound";
+import { useAuthContext } from "@asgardeo/auth-react";
+import { useEffect, useState, useCallback } from "react";
+import Land from "./components/Land";
+
+function App() {
+  const { section } = useViewContext();
+
+  const {
+    state,
+    signIn,
+    signOut,
+    getBasicUserInfo,
+    getIDToken,
+    getDecodedIDToken,
+    on,
+  } = useAuthContext();
+
+  const [derivedAuthenticationState, setDerivedAuthenticationState] =
+    useState(null);
+  const [hasAuthenticationErrors, setHasAuthenticationErrors] = useState(false);
+  const [hasLogoutFailureError, setHasLogoutFailureError] = useState();
+
+  useEffect(() => {
+    if (!state?.isAuthenticated) {
+      return;
+    }
+
+    (async () => {
+      const basicUserInfo = await getBasicUserInfo();
+      const idToken = await getIDToken();
+      const decodedIDToken = await getDecodedIDToken();
+
+      const derivedState = {
+        authenticateResponse: basicUserInfo,
+        idToken: idToken.split("."),
+        decodedIdTokenHeader: JSON.parse(atob(idToken.split(".")[0])),
+        decodedIDTokenPayload: decodedIDToken,
+      };
+
+      setDerivedAuthenticationState(derivedState);
+    })();
+  }, [state.isAuthenticated, getBasicUserInfo, getIDToken, getDecodedIDToken]);
+
+  // const handleLogin = useCallback(() => {
+  //   console.log("clicked log in");
+  //   setHasLogoutFailureError(false);
+  //   signIn().catch(() => console.log("asd"));
+  // }, [signIn]);
+
+  const handleLogin = () => {
+    signIn();
+  };
+
+  return (
+    <>
+      {state.isAuthenticated ? (
+        <div className="h-screen flex flex-row font-display divide-x">
+          <Sidebar />
+
+          {section === "Home" ? (
+            <Home />
+          ) : section === "Police" ? (
+            <Police />
+          ) : section === "Identity" ? (
+            <Identity />
+          ) : section === "Address" ? (
+            <Address />
+          ) : (
+            <NotFound />
+          )}
+        </div>
+      ) : (
+        <Land handleLogin={handleLogin} />
+      )}
+    </>
+  );
+}
+
+export default App;
