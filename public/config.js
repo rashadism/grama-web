4cfb7ba523ff48bce6f6ad5416fe7c0d4ef3d824
Rashad Sirajudeen <rashad@wso2.com>
2024-02-06 10:06:06 +0530

    hehe

--- public/config.js
+++ public/config.js
@@ -12,7 +12,7 @@ window.config = {
     signOutRedirectURL: "http://localhost:5173",
     police_endpoint: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/djxs/policehistory/endpoint-8080-5c6/v1.0",
     identity_endpoint: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/djxs/identitycheck-quc/idcheck-67a/v1.0",
-    address_endpoint: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/djxs/addresscheckapi/endpoint-9090-803/v1.0",
+    address_endpoint: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/djxs/addresscheckdevv2/endpoint-9090-803/v1.0",
     manager_endpoint: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/djxs/manager-v2/management-746/v1.0"
   }
 
